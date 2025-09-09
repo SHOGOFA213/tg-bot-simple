202072af12230f6c5a064089c0364e712b1eb5ab
SHOGOFA213 <shogofa39@gmail.com>
2025-09-09 21:00:26 +0300

    Первый коммит: добавлен tg-bot-simple

---
+++ README.md
@@ -0,0 +1,13 @@
+# Telegram Bot
+
+Простой Telegram-бот, написанный на Python с использованием библиотеки **pyTelegramBotAPI**.
+
+## Возможности
+- Команда `/start` — бот приветствует пользователя.  
+- Команда `/help` — показывает список доступных команд.  
+
+## Установка и запуск
+1. Склонировать репозиторий:
+
+   git clone https://github.com/SHOGOFA213/tg-bot-simple.git
+   cd tg-bot-simple
---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+import telebot
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+if not TOKEN:
+ raise RuntimeError(" .env =5F TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+ bot.reply_to(message, "@<25F! / F2>= ?5@2O= 1>F! 0?<H< /help")
+@bot.message_handler(commands=['help'])
+def help_cmd(message):
+ bot.reply_to(message, "/start 4 =0G0FP\n/help 4 ?><>IP")
+
+@bot.message_handler(commands=['hello'])
+def hello(message):
+    bot.reply_to(message, "Привет! Я твой бот 😊")
+
+
+@bot.message_handler(commands=['about'])
+def about(message):
+    bot.reply_to(
+        message,
+        "🤖 Бот: tg-bot-simple\n"
+        "Автор: Shogofa\n"
+        "Версия: 1.0\n"
+        "Назначение: учебный проект"
+    )
+
+if __name__ == "__main__":
+ bot.infinity_polling(skip_pending=True)
+
+
+
